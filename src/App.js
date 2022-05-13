492f5824906b60865deae28a722d4f7f7d0098ce
kasakvaish <kasakvaish02@gmail.com>
2022-05-13 22:29:16 +0530

    users

--- src/App.js
+++ src/App.js
@@ -6,15 +6,20 @@ import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import Contact from "./components/pages/Contact";
 import Navbar from "./components/layout/Navbar";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-        
-        <Home />
-        <About />
-        <Contact />
-        <Navbar />
+      <Routes>
+        <Route exact path="/" component={<Home />} />
+        <Route exact path="/about" component={<About />} />
+        <Route exact path="/contact" component={<Contact />} />
+      </Routes>
+      <Navbar />
     </div>
+    </BrowserRouter>
   );
 }
 
